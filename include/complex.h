817a96b57cfa59e5c5eb5c245b9377c707a8dd5f
LashaGOG <gogritchianilasha@gmail.com>
2023-12-09 17:36:10 +0100

    changed complex logic. Check with valgrind !

--- include/complex.h
+++ include/complex.h
@@ -7,31 +7,24 @@
              CHEN Virginie
 */
 
-
 #ifndef COMPLEX
 #define COMPLEX
 
-
-typedef struct _complex {
+typedef struct
+{
     double re;
     double im;
-}complex;
+} complex;
 
+complex create_complex(double re, double im);
+// void delete_complex(complex* z);
+void print_complex(complex z);
 
-complex* create_complex(double a, double b);
-void delete_complex(complex* z);
-void print_complex(complex* z);
-
-complex* add_complex(complex* z1, complex* z2);
-complex* minus_complex(complex* z1, complex* z2);
-complex* mult_complex(complex* z1, complex* z2);
-// complex* conjugate_complex(complex*z1);
-complex* divide_complex(complex* z1, complex* z2);
-double norme_complex(complex* z);
-
-
-
-
-
+complex add_complex(complex z1, complex z2);
+complex subtract_complex(complex z1, complex z2);
+complex multiply_complex(complex z1, complex z2);
+// // complex* conjugate_complex(complex*z1);
+complex divide_complex(complex z1, complex z2);
+double norm_complex(complex z);
 
 #endif // COMPLEX
--- src/main.c
+++ src/main.c
@@ -10,11 +10,20 @@
 
 int main()
 {
-    int n = 3;
-    complex **roots = nth_roots(n);
-    for (int i = 0; i < n - 1; i++)
-    {
-        print_complex(roots[i]);
-    }
+    complex z1 = create_complex(1.0, 2.0);
+    complex z2 = create_complex(3.0, 4.0);
+
+    complex sum = add_complex(z1, z2);
+    complex sub = subtract_complex(z1, z2);
+    complex mul = multiply_complex(z1, z2);
+    complex div = divide_complex(z1, z2);
+    double norme_z2 = norm_complex(z2);
+
+    print_complex(sum);
+    print_complex(sub);
+    print_complex(mul);
+    print_complex(div);
+    printf("%lf\n", norme_z2);
+
     return 0;
 }
