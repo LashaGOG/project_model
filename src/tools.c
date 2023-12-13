2e3a5aed794b89f21351aeb3f9271d38fcd181b4
Virgs <virgschen@gmail.com>
2023-12-13 21:40:41 +0100

    ok

--- src/tools.c
+++ src/tools.c
@@ -1,8 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <time.h>
-
 #include "../include/tools.h"
 
 /* AUTHORS : TAN Philippe
@@ -10,7 +5,4 @@
              CHEN Virginie
 */
 
-#ifndef TOOLS
-#define TOOLS
 
-#endif // TOOLS
